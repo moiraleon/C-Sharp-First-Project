12f5dce3bfffa4ba39ee9cc01124ba3fcc463b6a
Moira Leon <moira.leon2@gmail.com>
2021-10-19 18:43:45 -0600

    first app complete

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Again!");
+
+            //run with control +F5
         }
     }
 }
