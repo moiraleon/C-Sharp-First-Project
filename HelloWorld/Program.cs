d4d80fdb023f353d542e6319d8c4109142d5b27a
Moira Leon <moira.leon2@gmail.com>
2021-10-19 19:32:11 -0600

    Variables and Constants Demo

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,32 @@ namespace HelloWorld
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World Again!");
 
+
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A'; //char uses single quotes
+            string firstName = "Moira"; //string uses double quotes
+            bool isWorking = true;
+            //all of these variables can also be declared using var and Studio Code will detect the type-? however all numbers will default to integer type which is gererally safe to use
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+
+            //NOTES
             //run with control +F5
+            // to build an application command + B
+            //type cw then tab(twice) to get the console.writeline
+            //hovering over key words and using command + click opens the Assembly Browser
+            //command +x deletes a line
+            //templates are placed within curly braces and double quotes
         }
     }
 }
